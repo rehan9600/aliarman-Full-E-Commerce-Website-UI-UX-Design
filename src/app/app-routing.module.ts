cce43a180dfe8bba6ec043569999a0e3f53ce2ce
alrman_ali <aliarman9600@gmail.com>
2024-08-10 18:33:39 +0530

    update thecode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(m =>m.PagesModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m =>m.AuthModule),
+    // canActivate: [UserLoggedInGuard],
+    // providers: [UserLoggedInGuard]
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PagesComponent,
+  ],
   imports: [
     CommonModule,
-    PagesRoutingModule
+    PagesRoutingModule,
+    
+    SharedModule,
+    
+
   ]
 })
 export class PagesModule { }
