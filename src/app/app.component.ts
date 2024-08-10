7a672f00894d17a7120791a3ecb6fb86e4bde868
alrman_ali <aliarman9600@gmail.com>
2024-08-10 17:49:55 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Full-E-Commerce-Website-UI-UX-Design';
+}
