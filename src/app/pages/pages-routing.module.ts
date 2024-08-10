e9da37c08a6d737b866dbbc3d7c45ec10e18a436
alrman_ali <aliarman9600@gmail.com>
2024-08-10 18:32:47 +0530

    update thecode

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,22 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    // children[
+    //   {
+    //     path:''
+    //     Component:
+    //   },
+    // ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
